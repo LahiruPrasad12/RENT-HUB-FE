6c1e794a3b350760d61e538101f73d4f2ac48fa4
LahiruPrasad12 <lahirupr471@gmail.com>
2023-02-03 21:23:44 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://rent-hub-api.vercel.app/',
+  baseUrl: 'https://rent-hub-api.vercel.app',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://rent-hub-api.vercel.app/'
+  baseUrl: 'https://rent-hub-api.vercel.app'
 };
 
 /*
